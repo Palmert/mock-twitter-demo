e1a5ef486bc9b7be8ba66877457e76bb974179c8
tpalmer <thom.palmer@kwilt.it>
2016-12-18 14:29:43 -0500

    Create unit tests for UserLoginAttempt

--- app/src/test/java/com/thompalmer/mocktwitterdemo/domain/AttemptUserLoginTest.java
+++ app/src/test/java/com/thompalmer/mocktwitterdemo/domain/AttemptUserLoginTest.java
@@ -1,21 +1,14 @@
 package com.thompalmer.mocktwitterdemo.domain;
 
 import com.thompalmer.mocktwitterdemo.data.api.LocalTwitterServer;
-import com.thompalmer.mocktwitterdemo.data.api.UserSession;
+import com.thompalmer.mocktwitterdemo.data.api.TwitterService;
 import com.thompalmer.mocktwitterdemo.data.api.model.request.LoginRequest;
 import com.thompalmer.mocktwitterdemo.data.api.model.response.LoginResponse;
-import com.thompalmer.mocktwitterdemo.data.sharedpreference.AuthTokenPref;
-import com.thompalmer.mocktwitterdemo.data.sharedpreference.LongPreference;
-import com.thompalmer.mocktwitterdemo.data.sharedpreference.StringPreference;
-import com.thompalmer.mocktwitterdemo.data.sharedpreference.UserEmailPref;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
@@ -23,25 +16,44 @@ import java.util.Random;
 
 import io.reactivex.Observable;
 
+import static com.thompalmer.mocktwitterdemo.data.api.LocalTwitterServer.*;
+import static org.mockito.Mockito.*;
+
 public class AttemptUserLoginTest {
-    public static final String EMAIL = "email";
-    public static final String PASSWORD = "password";
-    public static final long AUTH_TOKEN = new Random().nextLong();
+    private static final String EMAIL = "email";
+    private static final String PASSWORD = "password";
+    private static final long AUTH_TOKEN = new Random().nextLong();
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Mock
-    LocalTwitterServer twitterService;
+    TwitterService twitterService;
     @Mock
-    UserSession userSession;
+    UserSessionPersister userSessionPersister;
 
     @InjectMocks AttemptUserLogin attemptUserLogin;
 
     @Test
     public void shouldSaveCredentialsOnSuccessfulLogin() {
-        Mockito.when(twitterService.login(LoginRequest.create(EMAIL, PASSWORD)))
+        when(twitterService.login(any(LoginRequest.class)))
                 .thenReturn(Observable.just(LoginResponse.success(EMAIL, AUTH_TOKEN)));
-        attemptUserLogin.execute(EMAIL, PASSWORD);
+
+        attemptUserLogin.execute(EMAIL, PASSWORD).subscribe();
+
+        verify(userSessionPersister).save(EMAIL, AUTH_TOKEN);
+    }
+
+    @Test
+    public void shouldNotSaveCredentialsOnLoginFailure() {
+        Observable<LoginResponse> loginFailureResponse =
+                Observable.just(LoginResponse.failure(ERROR_UNAUTHORIZED, MESSAGE_INVALID_PASSWORD));
+
+        when(twitterService.login(any(LoginRequest.class)))
+                .thenReturn(loginFailureResponse);
+
+        attemptUserLogin.execute(EMAIL, PASSWORD).subscribe();
+
+        verify(userSessionPersister, never()).save(EMAIL, AUTH_TOKEN);
     }
 }
